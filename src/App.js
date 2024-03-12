18d6f9f60db18950d0e7059866c8e2c8c9fcca0b
Ananya Parida <ananya71001@gmail.com>
2024-03-12 23:42:22 +0530

    Add date range picker and search options to the header.

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Hotel from "./pages/hotel/Hotel";
+
+function App() {
+  return (
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/hotels" element={<List/>} />
+      <Route path="/hotels/:id" element={<Hotel/>} />
+    </Routes>
+   </BrowserRouter>
+  );
+}
+
+export default App;
